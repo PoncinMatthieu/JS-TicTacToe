fc8c69bfac6ee3286e00e328332942677907872d
Poncin Matthieu <poncin.matthieu@gmail.com>
2013-09-11 17:46:10 +0200

    Improved HTTP dispatcher to send responses easily.

--- Server/dispatcher.js
+++ Server/dispatcher.js
@@ -10,65 +10,62 @@ var contentTypes = {
     '.js':   "text/javascript"
 };
 
-function send404(response)
-{
-    response.writeHead(404, {"Content-Type": "text/plain"});
-    response.write("404 Not Found\n");
-    response.end();
-}
-
-function send500(response, error)
-{
-    response.writeHead(500, {"Content-Type": "text/plain"});
-    response.write(error + "\n");
-    response.end();
-}
-
 function sendStaticRessource(response, filename)
 {
     fs.readFile(filename, "binary", function(err, file) {
 	if(err) {
-	    send500(response, err);
+	    module.exports.send500(response, err);
             return;
 	}
 
-      var headers = {};
-      var contentType = contentTypes[path.extname(filename)];
-      if (typeof contentType !== "undefined")
-	  headers["Content-Type"] = contentType;
-      response.writeHead(200, headers);
-      response.write(file, "binary");
-      response.end();
+	var headers = {};
+	var contentType = contentTypes[path.extname(filename)];
+	if (typeof contentType !== "undefined")
+	    headers["Content-Type"] = contentType;
+
+	module.exports.sendResponse(response, 200, headers, file, "binary");
     });
 }
 
 module.exports = {
-  dispatch: function(request, response, callbacks) {
-      // get uri
-      var uri = url.parse(request.url).pathname;
-      var file = path.join(process.cwd(), uri);
-      var exists = path.existsSync(file);
+    dispatch: function(request, response, callbacks) {
+	// get uri
+	var uri = url.parse(request.url).pathname;
+	var file = path.join(process.cwd(), uri);
+	var exists = path.existsSync(file);
 
-      util.puts("Dispatch request:");
-      util.puts("\turi: " + uri);
-      util.puts("\tmethod: " + request.method);
-      util.puts("\tstatusCode: " + request.statusCode);
+	if (exists && fs.statSync(file).isDirectory())
+	    file += '/index.html';
 
-      if (exists && fs.statSync(file).isDirectory())
-	  file += '/index.html';
+	// check if the request was mapped, if not we send the static ressource requested
+	if (typeof callbacks[uri] !== "undefined" && typeof callbacks[uri][request.method] !== "undefined") {
+	    callbacks[uri][request.method](request, response);
+	} else {
+	    path.exists(file, function(exists) {
+		// check if the path exists and send the static ressource
+		if(exists)
+		    sendStaticRessource(response, file);
+		else
+		    module.exports.send404(response);
+	    });
+	}
+    },
 
-      // check if the request was mapped, if not we send the static ressource requested
-      if (typeof callbacks[uri] !== "undefined" && typeof callbacks[uri][request.method] !== "undefined") {
-	  callbacks[uri][request.method](request, response);
-      } else {
-	  path.exists(file, function(exists) {
-	      // check if the path exists and send the static ressource
-	      if(exists)
-		  sendStaticRessource(response, file);
-	      else
-		  send404(response);
-	  });
-      }
-  }
+    send404: function(response) {
+	module.exports.sendResponse(response, 404, {"Content-Type": "text/plain"}, "404 Not Found\n");
+    },
+
+    send500: function(response, error) {
+	module.exports.sendResponse(response, 500, {"Content-Type": "text/plain"}, error + "\n");
+    },
+
+    sendResponse: function(response, code, headers, content, contentAttr) {
+	response.writeHeader(code, headers);
+	if (typeof contentAttr === "undefined")
+	    response.write(content);
+	else
+	    response.write(content, contentAttr);
+	response.end();
+    }
 };
 
--- server.js
+++ server.js
@@ -4,28 +4,28 @@ var http = require("http");
 
 var dispatcher = require("./Server/dispatcher");
 
+// creation of the callbacks
 var callbacks = {
     // a small hello world
     "/hello": {
 	"GET": function(request, response) {
-	    response.writeHeader(200, {"Content-Type": "text/plain"});
-	    response.write("Hello World");
-	    response.end();
+	    dispatcher.sendResponse(response, 200, {"Content-Type": "text/plain"}, "Hello World");
 	}
     },
 
     // serv the full content of the TicTacToe
     "/": {
 	"POST": function(request, response) {
-	    response.writeHeader(200, {"Content-Type": "text/plain"});
-	    response.write("Here is your post");
-	    response.end();
+	    dispatcher.sendResponse(response, 200, {"Content-Type": "text/plain"}, "Here is your post");
 	}
     }
 }
 
+// creation of the http server
 http.createServer(
     function(request, response) {
+	util.puts("Request " + request.method + ": " + request.url + ((request.statusCode != null) ? (" Status: " + request.StatusCode) : ""));
+
 	dispatcher.dispatch(request, response, callbacks);
     }).listen(8080);
 
